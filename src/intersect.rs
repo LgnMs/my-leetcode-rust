fa355a934ceb707140ffc52d4cc9ceb7fd164c39
LGN <792841450@qq.com>
2021-05-17 17:26:44 +0800

    update [350] 两个数组的交集 II

--- src/intersect.rs
+++ src/intersect.rs
@@ -2,7 +2,7 @@
  * @lc app=leetcode.cn id=1 lang=rust
  *
  * [350] 两个数组的交集 II
- * https://leetcode-cn.com/problems/single-number/
+ * https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/
  * - [哈希表]
  */
 use std::collections::HashMap;
