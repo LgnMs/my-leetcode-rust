37d4d98ece54592d2580b3b35fdce6e32393e935
LGN <792841450@qq.com>
2021-05-27 16:43:38 +0800

    update [98] 验证二叉搜索树

--- tree/is_valid_bst.rs
+++ tree/is_valid_bst.rs
@@ -9,6 +9,7 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use crate::tree_utils::TreeNode;
 
+// 递归方法解决
 pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
     fn is_bst(node: Option<&Rc<RefCell<TreeNode>>>, min: i64, max: i64) -> bool {
         match node {
@@ -26,11 +27,32 @@ pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
 
     is_bst(root.as_ref(), i64::MIN, i64::MAX)
 }
+// 中序遍历
+pub fn is_valid_bst2(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    let mut min = i64::MIN;
+    fn is_bst(node: Option<&Rc<RefCell<TreeNode>>>,min: &mut i64) -> bool {
+        if node.is_none() {
+            return true;
+        }
+        let mut bl = is_bst(node.unwrap().borrow().left.as_ref(), min);
+        println!("{}", node.unwrap().borrow().val);
+        if !bl {
+            return false;
+        }
+        if (node.unwrap().borrow().val as i64) < *min {
+            return false;
+        }
+        *min = node.unwrap().borrow().val as i64;
+        bl = is_bst(node.unwrap().borrow().right.as_ref(), min);
 
+        bl
+    }
+    is_bst(root.as_ref(),&mut min)
+}
 #[cfg(test)]
 mod test {
     use crate::tree_utils::TreeNode;
-    use crate::is_valid_bst::is_valid_bst;
+    use crate::is_valid_bst::{is_valid_bst, is_valid_bst2};
     use std::rc::Rc;
     use std::cell::RefCell;
 
@@ -40,7 +62,11 @@ mod test {
         tree.insert(1);
         tree.insert(3);
 
-        assert_eq!(is_valid_bst(Some(Rc::new(RefCell::new(tree)))), true);
+        let params = Some(Rc::new(RefCell::new(tree)));
+        let params2 = params.clone();
+
+        assert_eq!(is_valid_bst(params), true);
+        assert_eq!(is_valid_bst2(params2), true);
     }
     #[test]
     fn it_work_2() {
@@ -50,11 +76,19 @@ mod test {
         let right = tree.get_right();
         right.borrow_mut().insert(3);
         right.borrow_mut().insert(6);
+        let params = Some(Rc::new(RefCell::new(tree)));
+        let params2 = params.clone();
 
-        assert_eq!(is_valid_bst(Some(Rc::new(RefCell::new(tree)))), false);
+        assert_eq!(is_valid_bst(params), false);
+        assert_eq!(is_valid_bst2(params2), false);
     }
     #[test]
     fn it_work_3() {
+        //      5
+        //     / \
+        //    4   6
+        //       / \
+        //      3   7
         let mut tree = TreeNode::new(5);
         tree.insert(4);
         tree.insert(6);
@@ -62,6 +96,10 @@ mod test {
         right.borrow_mut().insert(3);
         right.borrow_mut().insert(7);
 
-        assert_eq!(is_valid_bst(Some(Rc::new(RefCell::new(tree)))), false);
+        let params = Some(Rc::new(RefCell::new(tree)));
+        let params2 = params.clone();
+
+        assert_eq!(is_valid_bst(params), false);
+        assert_eq!(is_valid_bst2(params2), false);
     }
 }
