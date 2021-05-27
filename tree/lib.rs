a80fde4964914210cd8a235a6ed454d106e7f2fb
LGN <792841450@qq.com>
2021-05-27 16:01:04 +0800

    add [144] 二叉树的前序遍历

--- tree/lib.rs
+++ tree/lib.rs
@@ -1,3 +1,4 @@
 pub mod tree_utils;
 mod max_depth;
 mod is_valid_bst;
+mod preorder_traversal;
---
+++ tree/preorder_traversal.rs
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=rust
+ *
+ * [144] 二叉树的前序遍历
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+ * ps: 在rust中使用迭代法太折磨了，我就不写了
+ */
+use std::rc::Rc;
+use std::cell::RefCell;
+use crate::tree_utils::TreeNode;
+
+pub fn preorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut nums = vec![];
+
+    fn traver(node: Option<&Rc<RefCell<TreeNode>>>, nums: &mut Vec<i32>) {
+        let node = match node {
+            None => {
+                return;
+            },
+            Some(x) => x,
+        };
+        nums.push(node.borrow().val);
+
+        if node.borrow().left.is_some() {
+            traver(node.borrow().left.as_ref(), nums);
+        }
+        if node.borrow().right.is_some() {
+            traver(node.borrow().right.as_ref(), nums);
+        }
+    }
+    traver(root.as_ref(), &mut nums);
+    nums
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::tree_utils::TreeNode;
+    use crate::preorder_traversal::preorder_traversal;
+    use std::rc::Rc;
+    use std::cell::RefCell;
+
+    #[test]
+    fn it_work_1() {
+        let mut tree = TreeNode::new(1);
+        tree.insert(2);
+
+        let tree = Some(Rc::new(RefCell::new(tree)));
+
+        assert_eq!(preorder_traversal(tree), vec![1, 2]);
+    }
+}
